8fee9cf98b2e55c6fc15354bcae5d4691ac2fe33
Xuanyao Fong <xuanyao.fong@gmail.com>
2020-01-01 16:55:43 +0800

    Adjust problem parameters to improve convergence

--- examples/SimpleGaussian/quadraticDrift.m
+++ examples/SimpleGaussian/quadraticDrift.m
@@ -2,6 +2,6 @@ function [driftVector] = quadraticDrift(xdata, timeStamp)
 %QUADRATICDRIFT Summary of this function goes here
 %   Detailed explanation goes here
 minPt = 50e-9;
-scaleVal = 1e16;
+scaleVal = 1e0;
 driftVector = - scaleVal .* (xdata - minPt.*ones(size(xdata)));
 end
